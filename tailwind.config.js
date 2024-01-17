1d4053458828dd821bc44994807d3cfb3077848d
Mike Vormwald <mvormwald@gmail.com>
2024-01-17 06:12:53 +0000

    update tailwind config for nicer content styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,34 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      typography: {
+      typography: (theme) => ({
         DEFAULT: {
           css: {
+            "--tw-prose-links": theme("colors.slate.700"),
+            "--tw-prose-links-hover": "#2925C3",
             img: {
               margin: "auto",
             },
+            a: {
+              color: "var(--tw-prose-links)",
+              fontWeight: theme("fontWeight.medium"),
+            },
+            "a:hover": {
+              color: "var(--tw-prose-links-hover)",
+              textDecorationColor: "var(--tw-prose-underline-hover)",
+            },
           },
         },
-      },
+      }), // Add a comma here
       colors: {
         brand: "#2982DD",
         "brand-dark": "#2925C3",
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 };
