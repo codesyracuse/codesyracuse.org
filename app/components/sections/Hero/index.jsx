14dad1a0ad9f615718287a146f449bbf19d0e69c
Mike Vormwald <mvormwald@gmail.com>
2024-03-24 12:02:09 -0400

    add normal "boring" hero & tweak designs

--- app/components/sections/Hero/index.jsx
+++ app/components/sections/Hero/index.jsx
@@ -7,16 +7,16 @@ import { pagePath } from "~/lib/utils";
 export function Hero({ image, title, description, cta, secondaryCta }) {
   return (
     <FullWidthLayout className="bg-brand-dark">
-      <div className="relative isolate pt-14">
-        <div
-          className="absolute inset-x-0 -top-10 -z-10 transform-gpu overflow-hidden blur-3xl"
+          <div className="relative isolate px-6 pt-14 lg:px-8">
+            <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/3 rotate-[50deg] bg-gradient-to-tr from-indigo-100 to-blue-400 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[80rem]"
+            className="relative overflow-hidden left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
           />
         </div>
@@ -76,7 +76,7 @@ export function Hero({ image, title, description, cta, secondaryCta }) {
 export function BoringHero({ image, title, description, cta, secondaryCta }) {
   return (
     <FullWidthLayout>
-      <div className="relative bg-gray-300">
+      <div className="relative mb-2 md:mb-6 bg-gray-300">
         {/* Decorative image and overlay */}
         {image && (
           <>
